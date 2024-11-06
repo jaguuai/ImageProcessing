d3b104bb1961c1778357027e89c2225687666b44
Ayşe Yılmaz <ayseeyyilmaz96@gmail.com>
2024-11-06 16:03:26 +0300

    update

--- practice6_OpenCV_change_image_color.py
+++ practice6_OpenCV_change_image_color.py
@@ -12,6 +12,25 @@ img_new=cv.merge((b,r,g))
 #yesil renkli elmayı göster
 cv.imshow("Green Apple",img_new)
 
-cv.waitKey(0)
-cv.destroyAllWindows
 
+
+#bu uygulamada renk kanallarını degıstırmenın uzerınde duruldu
+#Eger sarı renk isteseydik b,r,r yapabilirdik Ton ayarı için b,r-20,r gibi kombınasonlar denenebilir
+
+img_chess=cv.imread("ForExpImg/chess.jpg")
+bc,gc,rc=cv.split(img_chess)
+
+blue_chess=cv.merge((rc,gc,bc))
+cv.imshow("Blue Chess",blue_chess)
+cv.imshow("Blue Chanel",bc)
+cv.imshow("Green Chanel",gc)
+cv.imshow("Red Chanel",rc)
+
+#Kanal degeri varsa 1 yoksa 0 . 0 oldugunda siyah 1 oldugunda ıse beyaz gosterıcek
+cv.imwrite("ForExpImg/blue_chess.jpg",blue_chess)
+cv.waitKey(0)
+
+
+
+
+cv.destroyAllWindows
